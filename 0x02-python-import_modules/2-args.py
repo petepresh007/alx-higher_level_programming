856afecd1caf711b09e0d5f3c565de85cd960f48
peter precious <peterprecious370@gmail.com>
2023-11-02 15:28:45 -0700

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv
+    size = len(arguments) - 1
+
+    if size > 1:
+        print("{} arguments:".format(size))
+        for i in range(1, size + 1):
+            print("{}: {}".format(i, arguments[i]))
+    elif size == 0:
+        print("{} arguments.".format(size))
+    else:
+        print("{} arguments:".format(size))
+        print("{}: {}".format(size, arguments[i]))
