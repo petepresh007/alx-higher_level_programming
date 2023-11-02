c0da50eeed2b765c907168940d09d52c5dc77c62
peter precious <peterprecious370@gmail.com>
2023-11-02 15:38:36 -0700

    f

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,5 @@ if __name__ == "__main__":
         print("{} arguments:".format(size))
         for i in range(1, size + 1):
             print("{}: {}".format(i, arguments[i]))
-    elif size == 0:
+    if size == 0:
         print("{} arguments.".format(size))
-    else:
-        print("{} arguments:".format(size))
-        print("{}: {}".format(size, arguments[1]))
