ff26adcce0a44e36f839e47b335554ea6721c944
peter precious <peterprecious370@gmail.com>
2023-11-06 08:58:44 -0800

    sentence

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        return None
+    if sentence:
+        s_l = len(sentence)
+    else:
+        s_l = 0
+    return (s_l, sentence if not sentence else sentence[:1])
