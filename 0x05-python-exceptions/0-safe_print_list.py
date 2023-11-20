1705120389537dce34081bade946c44b768e39be
petepresh007 <peterprecious370@gmail.com>
2023-11-20 11:33:04 -0800

    IndexError

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+def safe_print_list(my_list=[], x=0):
+    counts = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            counts += 1
+        except IndexError:
+            break
+    print()
+    return counts
