f6c098a785731e0b10a8d0d09998ad496e91339f
petepresh007 <peterprecious370@gmail.com>
2023-11-27 14:02:21 -0800

    f fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """the class proper"""
     def __init__(self, width=0, height=0):
         """initialize rec"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
