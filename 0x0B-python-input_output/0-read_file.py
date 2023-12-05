27e42e6880efd0de06749c05d9e64f4f65011762
petepresh007 <peterprecious370@gmail.com>
2023-12-05 13:32:42 -0800

    readfile

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""aa function to read a file"""
+
+
+def read_file(filename=""):
+    """ File """
+
+    with open(filename, encoding="utf-8") as f:
+        read_file = f.read()
+        print(read_file)
