6fd274b4baff63b4c2903488248140663511cc2d
petepresh007 <peterprecious370@gmail.com>
2023-12-14 06:44:42 -0800

    fixed dic

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,6 +20,6 @@ class Base:
     def to_json_string(list_dictionaries):
         """ return json """
         if list_dictionaries is None or not list_dictionaries:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
