72f87b7ce740dc1b2fc4c0fd2f623e7d32c6bde1
petepresh007 <peterprecious370@gmail.com>
2023-12-14 01:26:58 -0800

    size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,16 @@ class Square(Rectangle):
         """ constructor """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """ get and set width """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """ print a string """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
