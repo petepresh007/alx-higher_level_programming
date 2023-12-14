fed03570310937d2271ab5cad0de7948ea5ce8ef
petepresh007 <peterprecious370@gmail.com>
2023-12-14 00:40:54 -0800

    fixed square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,3 +8,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ constructor """
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ print a string """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
