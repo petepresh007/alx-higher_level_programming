4f0eb742f5df4843a4e3c6211c5f537519fd1133
petepresh007 <peterprecious370@gmail.com>
2023-12-14 01:46:14 -0800

    fixed square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,6 +19,27 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        if args:
+            """ If *args is not empty, assign attributes using *args """
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
+
     def __str__(self):
         """ print a string """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
