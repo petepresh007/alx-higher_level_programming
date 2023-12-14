579157d2ebd052366cd99358e6f01a03e03f93d2
petepresh007 <peterprecious370@gmail.com>
2023-12-14 08:01:07 -0800

    fixed 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" class """
+
+
+class Student:
+    """ A student class """
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ dictionary representation of student """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: getattr(self, key) for key in attrs
+                    if hasattr(self, key)}
