d65b7fd16fd61b528c59888acad5ab29288fb95e
petepresh007 <peterprecious370@gmail.com>
2023-12-14 05:50:38 -0800

    rectangle update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -123,3 +123,12 @@ class Rectangle(Base):
                     self.x = v
                 elif k == "y":
                     self.y = v
+
+    def to_dictionary(self):
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
