5bbc06a23fe9b023fa145c2ef16af0732934991e
petepresh007 <peterprecious370@gmail.com>
2024-01-16 15:21:18 -0800

    fixed

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,5 @@
 -- display max temp
-SELECT `state`, MAX(`value`) AS `max_temp` FROM `temperatues` GROUP BY `state` ORDER BY `state`
+SELECT `state`, MAX(`value`) AS `max_temp`
+FROM `temperatues`
+GROUP BY `state`
+ORDER BY `state`
