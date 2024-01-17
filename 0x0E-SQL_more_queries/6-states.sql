504fd37a9815935f323d0c968cd53b89b390bb0e
petepresh007 <peterprecious370@gmail.com>
2024-01-17 13:41:57 -0800

    states and usa

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- script that creates db state
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT AUTO_INCREMENT UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
