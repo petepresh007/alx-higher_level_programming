b5187dd82af126fa17253eab4b4479896b2c7ae9
petepresh007 <peterprecious370@gmail.com>
2024-01-17 14:39:20 -0800

    f

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- select cities in states
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY cities.id ASC;
