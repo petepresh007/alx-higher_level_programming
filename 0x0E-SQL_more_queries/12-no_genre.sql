7f18dd5cfeb7bc274e0d36e387007b7c50e32e38
petepresh007 <peterprecious370@gmail.com>
2024-01-20 05:34:07 -0800

    fixed null

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- use join to show
+-- without genre link
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
