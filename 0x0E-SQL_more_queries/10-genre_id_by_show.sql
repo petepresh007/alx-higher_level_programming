343db2e978ff1370c6ae68b6b04a4ec298219f58
petepresh007 <peterprecious370@gmail.com>
2024-01-20 04:16:14 -0800

    fixe

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- All shows to be selected
 SELECT hbtn_0d_tvshows.tv_shows.title,
 hbtn_0d_tvshows.tv_show_genres.genre_id FROM hbtn_0d_tvshows.tv_shows
-NATURAL JOIN hbtn_0d_tvshows.tv_show_genres ORDER BY hbtn_0d_tvshows.tv_show_genres.genre_id ASC, hbtn_0d_tvshows.tv_shows.title ASC;
+NATURAL JOIN hbtn_0d_tvshows.tv_show_genres ORDER BY  hbtn_0d_tvshows.tv_shows.title ASC, hbtn_0d_tvshows.tv_show_genres.genre_id ASC;
