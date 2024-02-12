559f6f687f9d35b1af54f1eac15359b63aee68e3
petepresh007 <peterprecious370@gmail.com>
2024-02-12 07:23:00 -0800

    arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argument = process.argv;
+
+if (argument.length < 3) {
+  console.log('No argument');
+} else if (argument.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
