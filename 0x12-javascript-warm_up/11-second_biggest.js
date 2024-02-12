4753339e45c433ed8ea487d8105992158af247e6
petepresh007 <peterprecious370@gmail.com>
2024-02-12 13:52:06 -0800

    second highest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,20 @@
 #!/usr/bin/node
-const value = 
+const args = process.argv.slice(2).map(Number); // Convert arguments to integers
+
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  let max = -Infinity;
+  let secondMax = -Infinity;
+
+  for (let i = 0; i < args.length; i++) {
+    const num = args[i];
+    if (num > max) {
+      secondMax = max;
+      max = num;
+    } else if (num > secondMax && num !== max) {
+      secondMax = num;
+    }
+  }
+  console.log(secondMax !== -Infinity ? secondMax : 0);
+}
