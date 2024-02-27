183bb0c4ce66974ef1f0ba53ec6a90d0637271c7
petepresh007 <peterprecious370@gmail.com>
2024-02-27 03:16:52 -0800

    converter

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   return function (num) {
-    if (num < base) {
-      return num.toString();
-    }
-    return (num / base).converter(base) + (num % base).toString();
-  }
-}
+    return num.toString(base);
+  };
+};
