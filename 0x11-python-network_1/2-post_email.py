71c88c73a954a70df76719b51f086bc40cba0423
petepresh007 <peterprecious370@gmail.com>
2024-03-01 04:43:01 -0800

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script to post an email with the email
+suplied as an input
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    value = {"email": email}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        res = response.read()
+        print("Your email is: {}".format(res))
