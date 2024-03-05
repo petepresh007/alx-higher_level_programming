fa33ded3cf415bae8819525b8622e8483874ff4c
peter precious <peterprecious370@gmail.com>
2024-03-05 08:34:02 -0800

    fixed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # the size of the webpage
 curl -w '%{size_download}\n' -o /dev/null -s http://example.com
-
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # a Bash script that takes in a URL, sends a GET request to the UR
 curl -s "$1"
